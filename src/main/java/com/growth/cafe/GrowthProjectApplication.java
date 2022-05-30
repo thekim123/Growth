d5909f847661d866942752fb646135e7ba427453
Younghwan <tacdo12@naver.com>
2022-05-30 13:20:28 +0900

    환경세팅 완료

---
+++ src/main/java/com/growth/cafe/GrowthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.growth.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthProjectApplication.class, args);
+	}
+
+}
